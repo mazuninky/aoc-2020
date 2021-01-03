6dd73e0c3b5386c090b25678643fdf33b042c87e
Mazunin Konstantin <mazuninky@gmail.com>
2021-01-03 04:04:50 +0300

    Complete day 3 ruby part 2

--- day-3-ruby/main.rb
+++ day-3-ruby/main.rb
@@ -1,27 +1,28 @@
 forest = File.read("input.txt").split.map { |x| x.chars }
-EMPTY = "."
-TREE = "#"
-
-forest_size = forest.length
-forest_length = forest.first.length
-
-y = 0
-x = 0
-
-y_step = 1
-x_step = 3
 
 def calculate_index(arr, index)
   return index % arr.length 
 end
 
-tree_count = 0
-while y < forest_size
-  if forest[y][calculate_index(forest[y], x)] == TREE
-    tree_count += 1 
-  end 
-  x += x_step
-  y += y_step
+def check_path(x_step, y_step, forest)
+  forest_size = forest.length
+  forest_length = forest.first.length
+  tree_count = 0
+  x = 0
+  y = 0
+  while y < forest_size
+    if forest[y][calculate_index(forest[y], x)] == "#"
+      tree_count += 1 
+    end 
+    x += x_step
+    y += y_step
+  end
+  return tree_count
 end
 
-puts tree_count
+trees = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]].map do |pair|
+  x = pair.first
+  y = pair.last
+  check_path(x, y, forest)
+end
+puts trees.reduce(:*)
